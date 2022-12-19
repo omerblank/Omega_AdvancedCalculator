71660fe4e6c93b77e4504635ed8e5ab2aadee9de
omerblank <omerblank@gmail.com>
2022-12-19 09:42:05 +0200

    Fixing signed_operand function again.

---
+++ main.py
@@ -0,0 +1,10 @@
+# module to allow the user to use the calculator
+
+from Output.calculator import *
+
+# todo: when finishing merge all to master including main.py!!!!!!!!!!!!!!!
+# calculator()
+calculation()
+# print(signed_operand(reduce_minuses("~-~3")))
+# print(calculate("5!#*3@1+(~1)^4+((2))*((4^(~2.00)))"))
+# print(reduce_spaces("2+  3"))
